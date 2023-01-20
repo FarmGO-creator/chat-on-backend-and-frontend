11e69827f7264e6a5422acf8dcb33aab1b6efeda
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-20 23:06:14 +0600

    Написал логику получения сообщений и отправку сообщений

--- api/routes/messages.ts
+++ api/routes/messages.ts
@@ -1,13 +1,24 @@
 import express from 'express';
+import fileDb from "../fileDb";
+import {MessagesPost} from "../types";
 
 const messagesExpress = express.Router();
 
-messagesExpress.get('/', (req, res) => {
-  res.send('Сообщения');
+messagesExpress.get('/', async (req, res) => {
+  const messages = await fileDb.getMessages();
+  res.send(messages);
 });
 
-messagesExpress.post('/', (req, res) => {
-  res.send('Отправить')
+messagesExpress.post('/', async (req, res) => {
+  const message:MessagesPost = {
+    author: req.body.author,
+    message: req.body.message,
+  };
+
+  const saveMessage = await fileDb.postMessage(message);
+
+  res.send(saveMessage);
+
 });
 
 export default messagesExpress;
