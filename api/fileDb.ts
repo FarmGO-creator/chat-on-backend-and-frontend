3750cdd1a46a88a10282b7f714cc42290b40fcbe
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-20 23:05:32 +0600

    Создал базу данных

---
+++ api/fileDb.ts
@@ -0,0 +1,43 @@
+import {promises as fs} from "fs";
+import {Messages, MessagesPost} from "./types";
+import {randomUUID} from "crypto";
+
+const filename = 'db.json';
+let dataDb:Messages[] = [];
+
+const fileDb = {
+  async init() {
+    try {
+      const fileContent = await fs.readFile(filename);
+      dataDb = JSON.parse(fileContent.toString());
+    }catch (e) {
+      dataDb = [];
+    }
+  },
+
+  async getMessages() {
+    return dataDb
+  },
+
+  async postMessage(message: MessagesPost) {
+    const date = new Date();
+    const id = randomUUID();
+    const datetime = date.toISOString();
+
+    const newMessage:Messages = {
+      id,
+      datetime,
+      ...message
+    };
+
+    dataDb.push(newMessage);
+    await this.save();
+    return newMessage;
+  },
+
+  async save() {
+    return fs.writeFile(filename, JSON.stringify(dataDb));
+  }
+};
+
+export default fileDb;
