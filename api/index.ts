7207db83dd2a426a2102d60a47f33aeb984323ea
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-20 23:06:32 +0600

    Изменил старт сервера

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import messagesExpress from "./routes/messages";
 import cors from 'cors';
+import fileDb from "./fileDb";
 
 const app = express();
 const port = 8000;
@@ -10,7 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/messages', messagesExpress);
 
+const run = async () => {
+  await fileDb.init();
 
-app.listen(port, () => {
-  console.log('Server: ', port);
-});
+  app.listen(port, () => {
+    console.log('Server: ', port);
+  });
+}
+
+run().catch(console.error);
