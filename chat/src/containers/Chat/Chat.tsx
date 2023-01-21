64931a9557059cbe11fe9d6671b6c73df617d264
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-21 13:00:42 +0600

    Написал логику отправки сообщения и передал в форму

--- chat/src/containers/Chat/Chat.tsx
+++ chat/src/containers/Chat/Chat.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import {Box, Paper} from "@mui/material";
 import Form from "../../components/Form/Form";
 import Message from "../../components/Message/Message";
+import {useAppDispatch} from "../../app/hooks";
+import {MessageType} from "../../types";
+import {postMessages} from "../../store/chatThunk";
 
 const Chat = () => {
+  const dispatch = useAppDispatch();
+
+  const onSubmit = async (message: MessageType) => {
+    await dispatch(postMessages(message));
+  }
+
+
   return (
     <Paper elevation={3} sx={{width: 700, p: 1, minHeight: 700, display: 'flex', flexDirection: "column"}}>
       <Box component='div' sx={{height: 600, overflowY: 'scroll'}}>
@@ -11,7 +21,7 @@ const Chat = () => {
       </Box>
 
       <Box component='div' sx={{mt: 'auto'}}>
-        <Form/>
+        <Form onSubmit={onSubmit}/>
       </Box>
     </Paper>
   );
