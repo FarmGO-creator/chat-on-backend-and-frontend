3fa5598f9e27f16b461e46c5991acbf077c5709e
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-23 16:53:06 +0600

    Создал типы для сообщений на frontend

---
+++ chat/src/types.d.ts
@@ -0,0 +1,11 @@
+export interface MessageTypePost {
+  author: string;
+  message: string;
+}
+
+export interface MessageTypeFetch {
+  id: string;
+  datetime: string;
+  author: string;
+  message: string;
+}
