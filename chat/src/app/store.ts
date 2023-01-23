679515ef63208f1aa98a73af6ced7041bfc7e25a
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-23 16:52:17 +0600

    Подключил reducer к store

--- chat/src/app/store.ts
+++ chat/src/app/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {messagesReducer} from "../store/ChatSlice";
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    chat: messagesReducer,
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>
